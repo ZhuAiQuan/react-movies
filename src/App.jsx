4d08b2f346e7ae504301ba4055128b3f45fdb215
zhuaq <zhuaq@7000mall.com>
2022-03-30 15:40:42 +0800

    添加安全区以备移动端浏览器错误视图显示

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import BrowserRouter from "@/router";
 import { Provider } from 'react-redux';
-import store from './store'
+import store from './store';
+import {SafeArea} from 'antd-mobile'
 
 function App() {
   return (
     <Provider store={store}>
       <Router>
         <div className="App">
+          <SafeArea position="top" />
           <BrowserRouter />
+          <SafeArea position="bottom" />
         </div>
       </Router>
     </Provider>
