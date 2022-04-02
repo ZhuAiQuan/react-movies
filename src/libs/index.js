5a1bced6055cb31da2be3bcff0677310b8786199
zhuaq <zhuaq@7000mall.com>
2022-04-02 14:43:09 +0800

    chore: 环境判断

--- src/libs/index.js
+++ src/libs/index.js
@@ -2,13 +2,13 @@
  * @Description: xingp，yyds
  * @Author: zaq
  * @Date: 2022-04-01 14:11:57
- * @LastEditTime: 2022-04-02 10:10:05
+ * @LastEditTime: 2022-04-02 14:41:30
  * @LastEditors: zaq
  * @Reference: 
  */
 import HttpRequest from './axios'
 
-const baseUrl = '/api';
+const baseUrl = import.meta.env.DEV ? '/api' : 'https://m.maizuo.com/';
 const axios = new HttpRequest(baseUrl)
 
 export default axios
